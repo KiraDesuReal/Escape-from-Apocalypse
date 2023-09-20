26d0ffb535cec2778f63e46797a9617c5bb258ff
KiraDesuReal <kirill52055@yandex.ru>
2023-09-20 15:22:38 +0300

    Add loot in r1m1

--- data/scripts/server.lua
+++ data/scripts/server.lua
@@ -1461,7 +1461,7 @@ function CreateLootBoxForDeadScav(name, pos)
 
 	for l=1,count do
 
-		local Item = CreateNewObject{prototypeName = items[random(items_rand)], objName = "Item"..random(10000), belong = 1100}
+		local Item = CreateNewObject{prototypeName = items[random(items_rand)], objName = "ScavItem"..random(10000), belong = 1100}
 		local ItemId = GetEntityByID(Item)
 
 		if ChestId and ItemId then
@@ -1471,4 +1471,293 @@ function CreateLootBoxForDeadScav(name, pos)
 
 end
 
+function CreateGunBox(name, pos)
+
+	local gun = {"hornet01","specter01","pkt01","kord01","storm01","fagot01","maxim01","vector01","vulcan01","kpvt01","rapier01","flag01","rainmetal01","elephant01","odin01","omega01","bumblebee01","hammer01","hunterSideGun","mrakSideGun","big_swingfire01","cyclops01","octopus01","hailSideGun","hurricane01","rocketLauncher","zeusSideGun","marsSideGun"}
+	local gun_rand = getn(gun)
+
+	local gadgets = {"minePusher", "minePusher_1", "engineOilPusher", "nailsPusher", "Smoke", "additional_fuel_tank", "additional_torque", "additional_durability", "additional_stability", "add_damage_guns", "add_damage_energy", "add_damage_guns", "add_damage_energy", "add_damage_explosion", "someTurboAccelerationPusher", "firing_range_guns", "firing_rate_guns", "firing_rate_energy", "firing_rate_guns", "grouping_angle_guns", "firing_rate_energy", "grouping_angle_guns", "cooling_system_guns", "cooling_system_guns", "cooling_system_energy", "cooling_system_energy", "cooling_system_explosion"}
+	local gadgets_rand = getn(gadgets)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(3)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local notloot = random(100)
+	local count = random(2)
+	local affics = random(0,2)
+	local gun_r = random(2)
+
+	if notloot > 10 then
+		
+		if gun_r == 2 then
+			local Gun = CreateNewObject{prototypeName = gun[exrandom(gun_rand)], objName = "GunItem"..random(10000), belong = 1100}
+			local GunId = GetEntityByID(Gun)
+
+			local afflist = {}
+			afflist = CreateRandomAffixesForGun(affics)
+
+			if afflist ~= nil then
+				for i=1,getn(afflist) do
+					GunId:ApplyAffixByName(afflist[i])
+				end
+			end
+
+			if ChestId and GunId then
+				ChestId:AddChild(GunId)
+			end
+		end
+
+		for l=1,count do
+			local Gadgets = CreateNewObject{prototypeName = gadgets[exrandom(gadgets_rand)], objName = "GadgetItem"..random(10000), belong = 1100}
+			local GadgetsId = GetEntityByID(Gadgets)
+			if ChestId and GadgetsId then
+				ChestId:AddChild(GadgetsId)
+			end
+		end
+
+	end
+
+end
+
+function CreateMedBox(name, pos)
+
+	local medical = {"item_medical_tools", "item_naci", "item_h2o2", "item_suringe", "item_c6h8o6", "item_aquapeps", "item_oftalmaskop", "item_ledx"}
+	local medical_items = getn(medical)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(4)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local notloot = random(100)
+	local count = random(2)
+
+	if notloot > 10 then
+		
+		for l=1,count do
+			local Item = CreateNewObject{prototypeName = medical[exrandom(medical_items)], objName = "Item"..random(10000), belong = 1100}
+			local ItemId = GetEntityByID(Item)
+			if ChestId and ItemId then
+				ChestId:AddChild(ItemId)
+			end
+		end
+
+	end
+
+end
+
+function CreateTehnoBox(name, pos)
+
+	local use = {"scrap_metal_use", "oil_use", "machinery_use", "fuel_nil_use", "fuel_full_use", "electronics_use"}
+	local use_items = getn(use)
+
+	local building = {"item_scotch", "item_nails", "item_nuts", "item_insulation", "item_screws", "scrap_metal", "doski", "item_bolts", "item_hose", "details", "item_plex", "item_parts", "item_poheram", "item_tube", "item_kek", "machinery", "item_military_tube", "item_thermometer", "item_pena", "item_datchik"}
+	local building_items = getn(building)
+
+	local energy = {"item_battery_d", "item_battery_aa", "item_accum", "item_powerbank", "item_green_battery", "item_car_battery", "item_cyclon", "item_tank_battery"}
+	local energy_items = getn(energy)
+
+	local flammable = {"item_spich", "item_hunter_spich", "item_lighter", "item_wd40_100", "item_zibbo", "oil", "item_survl", "item_wd40_400", "fuel", "item_trotile", "item_dry", "item_propan", "item_prisadka", "item_termit"}
+	local flammable_items = getn(flammable)
+
+	local household = {"item_soap", "item_paste", "item_salt", "item_tb", "item_toothpaste", "item_soda", "item_paper", "item_alkani", "item_hlor", "book"}
+	local household_items = getn(household)
+
+	local other = {"bottle", "item_filter", "item_waterfilter", "item_paracord", "item_airfilter"}
+	local other_items = getn(other)
+
+	local tools = {"item_roulet", "item_wrench", "item_screw", "item_pliers", "item_screw_flat", "item_nippers", "item_metalscissors", "item_pliers_round", "item_leatherman", "item_screw_flat_long", "item_toolset", "item_awl", "item_fullmaster", "item_sewing_kit", "item_handrill", "item_buldex", "item_ratchet_wrench", "item_pipe_wrench"}
+	local tools_items = getn(tools)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(5)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local notloot = random(100)
+	local count = random(4)
+	local useItem = random(2)
+
+	if notloot > 10 then
+
+		if useItem == 2 then
+			local UseItem = CreateNewObject{prototypeName = use[exrandom(use_items)], objName = "UseItem"..random(10000), belong = 1100}
+			local UseItemId = GetEntityByID(UseItem)
+			if ChestId and UseItemId then
+				ChestId:AddChild(UseItemId)
+			end
+		end
+		
+		for l=1,count do
+			local items = {building[random(building_items)], energy[exrandom(energy_items)], flammable[random(flammable_items)], household[random(household_items)], other[exrandom(other_items)], tools[random(tools_items)]}
+			local items_random = getn(items)
+
+			local Item = CreateNewObject{prototypeName = items[random(items_random)], objName = "Item"..random(10000), belong = 1100}
+			local ItemId = GetEntityByID(Item)
+			if ChestId and ItemId then
+				ChestId:AddChild(ItemId)
+			end
+		end
+
+	end
+
+end
+
+function CreateToolsBox(name, pos)
+
+	local building = {"item_scotch", "item_nails", "item_nuts", "item_insulation", "item_screws", "scrap_metal", "doski", "item_bolts", "item_hose", "details", "item_plex", "item_parts", "item_poheram", "item_tube", "item_kek", "machinery", "item_military_tube", "item_thermometer", "item_pena", "item_datchik"}
+	local building_items = getn(building)
+
+	local energy = {"item_battery_d", "item_battery_aa", "item_accum", "item_powerbank", "item_green_battery", "item_cyclon"}
+	local energy_items = getn(energy)
+
+	local flammable = {"item_spich", "item_hunter_spich", "item_lighter", "item_wd40_100", "item_zibbo", "oil", "item_survl", "item_wd40_400", "fuel", "item_trotile", "item_dry", "item_termit"}
+	local flammable_items = getn(flammable)
+
+	local other = {"bottle", "item_filter", "item_waterfilter", "item_paracord"}
+	local other_items = getn(other)
+
+	local tools = {"item_roulet", "item_wrench", "item_screw", "item_pliers", "item_screw_flat", "item_nippers", "item_metalscissors", "item_pliers_round", "item_leatherman", "item_screw_flat_long", "item_toolset", "item_awl", "item_fullmaster", "item_sewing_kit", "item_handrill", "item_buldex", "item_ratchet_wrench", "item_pipe_wrench"}
+	local tools_items = getn(tools)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(6)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local notloot = random(100)
+	local count = random(3)
+
+	if notloot > 10 then
+		
+		for l=1,count do
+			local items = {building[random(building_items)], energy[exrandom(energy_items)], flammable[random(flammable_items)], other[exrandom(other_items)], tools[random(tools_items)]}
+			local items_random = getn(items)
+
+			local Item = CreateNewObject{prototypeName = items[random(items_random)], objName = "Item"..random(10000), belong = 1100}
+			local ItemId = GetEntityByID(Item)
+			if ChestId and ItemId then
+				ChestId:AddChild(ItemId)
+			end
+		end
+
+	end
+
+end
+
+function CreateValuableBox(name, pos)
+
+	local item = {"item_disk", "item_manual", "item_flashdrive", "item_diary_s", "item_diary", "item_ssd", "item_disk_exmachina", "item_sas", "item_rozvidka",
+					"item_chain", "shkatulka", "item_ex", "item_teapon", "item_cat", "item_rolex", "item_chain_gold", "item_woodclock", "item_chiken", "item_skullring", "item_lion", "item_bitcoin",
+					"item_key_gate_thetown", "item_key_gate_r1m3"}
+	local item_rand = getn(item)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(7)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local Item = CreateNewObject{prototypeName = item[random(item_rand)], objName = "Item"..random(10000), belong = 1100}
+	local ItemId = GetEntityByID(Item)
+
+	if ChestId and ItemId then
+		ChestId:AddChild(ItemId)
+	end
+
+end
+
+function CreateElectronicsBox(name, pos)
+
+	local electronics = {"item_usb", "item_wires", "item_tplug", "item_dvd", "item_lump", "item_rele", "item_cpu", "item_svech", "item_kondesators", "item_magnet", "item_energo_lump", "item_electronics_components", "item_phone", "item_ultra_lump", "item_cooler", "item_gazan", "item_geiger", "item_plate", "item_cable", "item_helix", "item_hdd", "item_drill", "item_lcd", "item_military_cable", "electronics", "item_bp", "item_engine", "item_iridiym", "item_tetris", "item_gpu"}
+	local electronics_items = getn(electronics)
+
+	local Chest = CreateNewObject{prototypeName = "someChest", objName = name.."Chest"}	
+	local ChestId = GetEntityByID(Chest)
+	ChestId:SetPosition(pos)
+	ChestId:SetSkin(1)
+
+	local ChestPos = ChestId:GetPosition()
+	ChestPos.y = ChestPos.y + 1
+	ChestId:SetPosition(ChestPos)
+
+	local OpacityItem = CreateNewObject{prototypeName = "item_opacity", objName = "OpacityItem"..random(10000), belong = 1100}
+	local OpacityItemId = GetEntityByID(OpacityItem)
+
+	if ChestId and OpacityItemId then
+		ChestId:AddChild(OpacityItemId)
+	end
+
+	local notloot = random(100)
+	local count = random(3)
+
+	if notloot > 10 then
+		
+		for l=1,count do
+			local Item = CreateNewObject{prototypeName = electronics[exrandom(electronics_items)], objName = "Item"..random(10000), belong = 1100}
+			local ItemId = GetEntityByID(Item)
+			if ChestId and ItemId then
+				ChestId:AddChild(ItemId)
+			end
+		end
+
+	end
+
+end
 
